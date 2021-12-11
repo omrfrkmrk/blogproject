df6b27cc87810a7904c67150d930bfc9a4f6b738
omrfrkmrk <omer_6042@hotmail.com>
2021-12-12 00:07:19 +0300

    Topbar, Header and Sidebar added(UI)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import TopBar from "./topbar/TopBar";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <p>Test</p>
-    </div>
+    <>
+      <TopBar/>
+      <Home/>
+    </>
   );
 }
 
